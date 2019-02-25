5e4d8be1c962cf5f5b6673ef88bddf430e7be0d1
David O'Brien <me@david-obrien.net>
2019-02-25 21:45:30 +1100

    Pass params to windows script

--- deploy-windowsvm.sh
+++ deploy-windowsvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ADMINNAME="windowsadmin"
+ADMINPASSWORD="P@ssw0rd123!"
 
-az vm create --image Win2019Datacenter --admin-username ${ADMINNAME} --location australiasoutheast --name windowstest --resource-group ${RGNAME} --size Standard_D3_v2 --vnet-name ${VNETNAME} --subnet management --public-ip-address "" --nsg "" --output table
+az vm create --image Win2019Datacenter --admin-username ${ADMINNAME} --admin-password ${ADMINPASSWORD} --location australiasoutheast --name windowstest --resource-group $1 --size Standard_D3_v2 --vnet-name $2 --subnet management --public-ip-address "" --nsg "" --output table
