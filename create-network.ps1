b35e12807707fff249b4bd7b2f0f2ce079a2606f
David O'Brien <me@david-obrien.net>
2019-02-25 21:54:19 +1100

    Use AzureRM module

--- create-network.ps1
+++ create-network.ps1
@@ -4,36 +4,36 @@ param (
   [string]$vnetName = "demovnet"
 )
 
-New-AzResourceGroup -Name $rgName -Location "Australia East"
+New-AzureRmResourceGroup -Name $rgName -Location "Australia East"
 
-$vnet = New-AzVirtualNetwork -Name $vnetName -AddressPrefix 192.168.0.0/16 -ResourceGroupName $rgName -Location "Australia East"
+$vnet = New-AzureRmVirtualNetwork -Name $vnetName -AddressPrefix 192.168.0.0/16 -ResourceGroupName $rgName -Location "Australia East"
 
-$subnetConfigGw = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix 192.168.0.0/27
+$subnetConfigGw = New-AzureRmVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix 192.168.0.0/27
 $vnet.Subnets.Add($subnetConfigGw)
-Set-AzVirtualNetwork -VirtualNetwork $vnet
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
 
-$subnetConfigJb = New-AzVirtualNetworkSubnetConfig -Name jumpbox -AddressPrefix 192.168.1.0/29
+$subnetConfigJb = New-AzureRmVirtualNetworkSubnetConfig -Name jumpbox -AddressPrefix 192.168.1.0/29
 $vnet.Subnets.Add($subnetConfigJb)
-Set-AzVirtualNetwork -VirtualNetwork $vnet
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
 
-$subnetConfigMgmt = New-AzVirtualNetworkSubnetConfig -Name management -AddressPrefix 192.168.2.0/24
+$subnetConfigMgmt = New-AzureRmVirtualNetworkSubnetConfig -Name management -AddressPrefix 192.168.2.0/24
 $vnet.Subnets.Add($subnetConfigMgmt)
-Set-AzVirtualNetwork -VirtualNetwork $vnet
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
 
-$rdp = New-AzNetworkSecurityRuleConfig -Name "allow-RDP-from-jumpbox" -SourcePortRange * -Protocol TCP -SourceAddressPrefix "192.168.2.0/24" -Access Allow -Priority 110 -Direction Inbound -DestinationPortRange 3389 -DestinationAddressPrefix *
-$ssh = New-AzNetworkSecurityRuleConfig -Name "allow-SSH-from-internet" -SourcePortRange * -Protocol TCP -SourceAddressPrefix Internet -Access Allow -Priority 120 -Direction Inbound -DestinationPortRange 22 -DestinationAddressPrefix *
+$rdp = New-AzureRmNetworkSecurityRuleConfig -Name "allow-RDP-from-jumpbox" -SourcePortRange * -Protocol TCP -SourceAddressPrefix "192.168.2.0/24" -Access Allow -Priority 110 -Direction Inbound -DestinationPortRange 3389 -DestinationAddressPrefix *
+$ssh = New-AzureRmNetworkSecurityRuleConfig -Name "allow-SSH-from-internet" -SourcePortRange * -Protocol TCP -SourceAddressPrefix Internet -Access Allow -Priority 120 -Direction Inbound -DestinationPortRange 22 -DestinationAddressPrefix *
 
-New-AzNetworkSecurityGroup -Name management -SecurityRules $rdp -ResourceGroupName $rgName -Location "Australia East"
-New-AzNetworkSecurityGroup -Name jumpbox -SecurityRules $ssh -ResourceGroupName $rgName -Location "Australia East"
+New-AzureRmNetworkSecurityGroup -Name management -SecurityRules $rdp -ResourceGroupName $rgName -Location "Australia East"
+New-AzureRmNetworkSecurityGroup -Name jumpbox -SecurityRules $ssh -ResourceGroupName $rgName -Location "Australia East"
 
-$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
+$vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
 
-$nsg = Get-AzNetworkSecurityGroup -Name jumpbox -ResourceGroupName $rgName
-$subnet = Get-AzVirtualNetworkSubnetConfig -Name jumpbox -VirtualNetwork $vnet
-$vnetConfig = Set-AzVirtualNetworkSubnetConfig -Name jumpbox -VirtualNetwork $vnet -NetworkSecurityGroup $nsg -AddressPrefix $subnet.AddressPrefix
-Set-AzVirtualNetwork -VirtualNetwork $vnetConfig
+$nsg = Get-AzureRmNetworkSecurityGroup -Name jumpbox -ResourceGroupName $rgName
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name jumpbox -VirtualNetwork $vnet
+$vnetConfig = Set-AzureRmVirtualNetworkSubnetConfig -Name jumpbox -VirtualNetwork $vnet -NetworkSecurityGroup $nsg -AddressPrefix $subnet.AddressPrefix
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnetConfig
 
-$nsg = Get-AzNetworkSecurityGroup -Name management -ResourceGroupName $rgName
-$subnet = Get-AzVirtualNetworkSubnetConfig -Name management -VirtualNetwork $vnet
-$vnetConfig = Set-AzVirtualNetworkSubnetConfig -Name management -VirtualNetwork $vnet -NetworkSecurityGroup $nsg -AddressPrefix $subnet.AddressPrefix
-Set-AzVirtualNetwork -VirtualNetwork $vnetConfig
+$nsg = Get-AzureRmNetworkSecurityGroup -Name management -ResourceGroupName $rgName
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name management -VirtualNetwork $vnet
+$vnetConfig = Set-AzureRmVirtualNetworkSubnetConfig -Name management -VirtualNetwork $vnet -NetworkSecurityGroup $nsg -AddressPrefix $subnet.AddressPrefix
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnetConfig
