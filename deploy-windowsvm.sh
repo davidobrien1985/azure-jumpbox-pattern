04175003e4dc769a1d5af02a7f5c1925c9f2c306
David O'Brien <me@david-obrien.net>
2019-02-25 22:29:17 +1100

    fix location

--- deploy-windowsvm.sh
+++ deploy-windowsvm.sh
@@ -3,4 +3,4 @@
 ADMINNAME="windowsadmin"
 ADMINPASSWORD="P@ssw0rd123!"
 
-az vm create --image Win2019Datacenter --admin-username ${ADMINNAME} --admin-password ${ADMINPASSWORD} --location australiasoutheast --name windowstest --resource-group $1 --size Standard_D3_v2 --vnet-name $2 --subnet management --public-ip-address "" --nsg "" --output table
+az vm create --image Win2019Datacenter --admin-username ${ADMINNAME} --admin-password ${ADMINPASSWORD} --location australiaeast --name windowstest --resource-group $1 --size Standard_D3_v2 --vnet-name $2 --subnet management --public-ip-address "" --nsg "" --output table
