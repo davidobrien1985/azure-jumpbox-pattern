1848e9f382e61be97c40128dcb1ebe3ec2957834
David O'Brien <me@david-obrien.net>
2019-02-26 10:22:47 +1100

    Add a sleep to give the VM some time

--- deploy-jumpbox.sh
+++ deploy-jumpbox.sh
@@ -4,4 +4,6 @@ ADMINNAME="jumpboxadmin"
 
 az vm create --image UbuntuLTS --generate-ssh-keys --admin-username ${ADMINNAME} --location australiaeast --name jumpbox --resource-group $1 --size Standard_D3_v2 --vnet-name $2 --subnet jumpbox --nsg "" --output table
 
+sleep 15
+
 az vm extension set --publisher Microsoft.Azure.ActiveDirectory.LinuxSSH --name AADLoginForLinux --resource-group $1 --vm-name jumpbox
